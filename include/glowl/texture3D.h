139c1d1b9a00bc5654dc0106867c8a57f863933f
invor <michael.becher.1990@googlemail.com>
2015-11-06 18:12:12 +0100

    Added 3d texture object
    Sorry, not tested yet

--- include/glowl/texture3D.h
+++ include/glowl/texture3D.h
@@ -1,35 +1,25 @@
-#ifndef texture2D_h
+#ifndef texture3D_h
 #define texture2D_h
 
 #include "texture.h"
 
-
 /**
-* \class Texture2D
+* \class Texture3D
 *
-* \brief Encapsulates basic 2D texure functionality.
+* \brief Encapsulates basic 3D texure functionality.
 *
-* This class encapsulates basic 2D functionality including creation of a 2D texture,
+* This class encapsulates basic 3D functionality including creation of a 3D texture,
 * as well as setting parameters and binding it.
 *
 * \author Michael Becher
 *
 * \date 6th November 2015
 */
-class Texture2D : public Texture
+class Texture3D : public Texture
 {
 public:
-	/**
-	* \brief Constructor that creates and loads a 2D texture.
-	* \param internal_format Specifies the internal format of the texture (e.g. GL_RGBA32F)
-	* \param dim_x Specifies the width of the texture in pixels.
-	* \param dim_y Specifies the height of the texture in pixels.
-	* \param format Specifies the format of the texture (e.g. GL_RGBA)
-	* \param type Specifies the type of the texture (e.g. GL_FLOAT)
-	* \param data Pointer to the actual texture data.
-	*/
-	Texture2D(std::string name, GLint internal_format, unsigned int width, unsigned int height, GLenum format, GLenum type, GLvoid * data);
-	Texture2D(const Texture2D &) = delete;
+	Texture3D(std::string name, GLint internal_format, unsigned int width, unsigned int height, unsigned int depth, GLenum format, GLenum type, GLvoid * data);
+	Texture3D(const Texture3D &) = delete;
 
 	/**
 	* \brief Bind the texture.
@@ -50,12 +40,12 @@ public:
 	template<typename Container>
 	void texParameteri(const Container& pname_param_pairs)
 	{
-		glBindTexture(GL_TEXTURE_2D, m_handle);
+		glBindTexture(GL_TEXTURE_3D, m_handle);
 
 		for(auto& pname_param : pname_param_pairs)
-			glTexParameteri(GL_TEXTURE_2D, pname_param.first, pname_param.second);
+			glTexParameteri(GL_TEXTURE_3D, pname_param.first, pname_param.second);
 
-		glBindTexture(GL_TEXTURE_2D, 0);
+		glBindTexture(GL_TEXTURE_3D, 0);
 	}
 
 	/**
@@ -63,10 +53,11 @@ public:
 	* \note Will not work if load() hasn't been called on this object yet.
 	* \param width Specifies the new width of the texture in pixels.
 	* \param height Specifies the new height of the texture in pixels.
+	* \param depth Specifies the new depth of the texture in pixels.
 	* \param data Pointer to the new texture data.
 	* \return Returns true if the texture was succesfully created, false otherwise
 	*/
-	bool reload(unsigned int width, unsigned int height, GLvoid * data);
+	bool reload(unsigned int width, unsigned int height, unsigned int depth, GLvoid * data);
 
 	/**
 	* \brief Reload the texture with any new format, type and size.
@@ -74,20 +65,22 @@ public:
 	* \param internal_format Specifies the new internal format of the texture (e.g. GL_RGBA)
 	* \param width Specifies the new width of the texture in pixels.
 	* \param height Specifies the new height of the texture in pixels.
+	* \param depth Specifies the new depth of the texture in pixels
 	* \param format Specifies the new format of the texture (e.g. GL_RGBA32F)
 	* \param type Specifies the new type of the texture (e.g. GL_FLOAT)
 	* \param data Pointer to the new texture data.
 	* \return Returns true if the texture was succesfully created, false otherwise
 	*/
-	bool reload(GLenum internal_format, unsigned int width, unsigned int height, GLenum format, GLenum type, GLvoid * data);
+	bool reload(GLenum internal_format, unsigned int width, unsigned int height, unsigned int depth, GLenum format, GLenum type, GLvoid * data);
 
 	unsigned int getWidth();
-
 	unsigned int getHeight();
+	unsigned int getDepth();
 
 private:
 	unsigned int m_width;
 	unsigned int m_height;
+	unsigned int m_depth;
 };
 
 #endif
---
+++ src/texture3D.cpp
@@ -0,0 +1,69 @@
+#include "texture3D.h"
+
+#include <cassert>
+
+Texture3D::Texture3D(std::string name, GLint internal_format, unsigned int width, unsigned int height, unsigned int depth, GLenum format, GLenum type, GLvoid * data)
+	: Texture(name,internal_format,format,type), m_width(width), m_height(height), m_depth(depth)
+{
+	glGenTextures(1, &m_handle);
+	assert(m_handle > 0);
+
+	glBindTexture(GL_TEXTURE_3D, m_handle);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage3D(GL_TEXTURE_3D, 0, internal_format, m_width, m_height, m_depth, 0, format, type, data);
+	glBindTexture(GL_TEXTURE_3D,0);
+
+	if (glGetError() == GL_NO_ERROR)
+	{
+		// "Do something cop!"
+	}
+}
+
+void Texture3D::bindTexture() const
+{
+	glBindTexture(GL_TEXTURE_3D, m_handle);
+}
+
+void Texture3D::texParameteri(GLenum pname, GLenum param)
+{
+	glBindTexture(GL_TEXTURE_3D, m_handle);
+	glTexParameteri(GL_TEXTURE_3D, pname, param);
+	glBindTexture(GL_TEXTURE_3D,0);
+}
+
+bool Texture3D::reload(unsigned int width, unsigned int height, unsigned int depth, GLvoid * data)
+{
+	glBindTexture(GL_TEXTURE_3D, m_handle);
+	glTexImage3D(GL_TEXTURE_3D, 0, m_internal_format, m_width, m_height, m_depth, 0, m_format, m_type, data);
+	glBindTexture(GL_TEXTURE_3D,0);
+}
+
+bool Texture3D::reload(GLenum internal_format, unsigned int width, unsigned int height, unsigned int depth, GLenum format, GLenum type, GLvoid * data)
+{
+	m_internal_format = internal_format;
+	m_format = format;
+	m_type = type;
+
+	glBindTexture(GL_TEXTURE_3D, m_handle);
+	glTexImage3D(GL_TEXTURE_3D, 0, internal_format, width, height, depth, 0, format, type, data);
+	glBindTexture(GL_TEXTURE_3D, 0);
+}
+
+unsigned int Texture3D::getWidth()
+{
+	return m_width;
+}
+
+unsigned int Texture3D::getHeight()
+{
+	return m_height;
+}
+
+unsigned int Texture3D::getDepth()
+{
+	return m_depth;
+}
